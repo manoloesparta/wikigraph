d5d602e410261881a7d466bcaa3d3bbb6453b782
manolo esparza <manoloesparta@gmail.com>
2020-03-18 12:41:18 -0700

    update

--- collect.py
+++ collect.py
@@ -7,24 +7,37 @@ WIKIPEDIA = 'https://en.wikipedia.org/'
 PREFIX = '/wiki/'
 DATADIR = 'data/'
 
-def get_neighbors(article):
+
+def visit(article):
     url = WIKIPEDIA + PREFIX + article
-    if not os.path.exists(DATADIR + article):        
+
+    if not os.path.exists(DATADIR + article):
+        print(f'Downloading {article}')
+
         source = requests.get(url)
         soup = BeautifulSoup(source.content, 'lxml')
         
         links = soup.find_all('a', attrs={ 'href': re.compile('^/wiki/[a-zA-Z0-9]+$') })
         res = [ link.get('href').split('/')[-1] for link in links ]
+        final = list(dict.fromkeys(res))
         
-        with open(DATADIR + article, 'w+') as f:
-            f.write(',\n'.join(res))
+        with open(DATADIR + article, 'w+') as file:
+            file.write(',\n'.join(final))
 
-    print(article + ' Already exist')        
-        
+        return final
+    
+    print(f'{article} downloaded')        
+
+    with open(DATADIR + article, 'r') as file:
+        content = file.read()
+        return content.split(',\n')
 
 if __name__ == "__main__":
 
     if not os.path.exists(DATADIR):
         os.mkdir(DATADIR)
         
-    res = get_neighbors('Mexico')
+    res = visit('Mexico')
+    for i in res:
+        for j in visit(i):
+            visit(j)
