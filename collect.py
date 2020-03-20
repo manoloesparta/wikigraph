a442c8ff67d2249c2c855b5ccd51962dec16752d
manolo esparza <manoloesparta@gmail.com>
2020-03-19 21:35:23 -0700

    update

--- collect.py
+++ collect.py
@@ -3,48 +3,61 @@ import os
 import random
 import requests
 import concurrent.futures
-from bs4 import BeautifulSoup
 
 WIKIPEDIA = 'https://en.wikipedia.org/'
 PREFIX = '/wiki/'
 DATADIR = 'data/'
+cache = {}
+
+
+def cleanstr(string):
+    return (string.split('/')[-1])[:-1]
 
 
 def visit(article):
+    global cache
     url = WIKIPEDIA + PREFIX + article
 
     if not os.path.exists(DATADIR + article):
         print(f'Downloading {article}')
+        source = str(requests.get(url).content)
+        pattern = re.compile(r'\"/wiki/[a-zA-Z0-9_()-.!\']+\"')
+        links = pattern.findall(source)
 
-        source = requests.get(url)
-        soup = BeautifulSoup(source.content, 'lxml')
-        
-        links = soup.find_all('a', attrs={ 'href': re.compile('^/wiki/[a-zA-Z0-9_()-.!\']+$') })
-        res = [ link.get('href').split('/')[-1] for link in links ]
+        res = [ cleanstr(link) for link in links ]
         final = list(dict.fromkeys(res))
 
         with open(DATADIR + article, 'w+') as file:
             file.write(',\n'.join(final))
 
+        cache[article] = final
         return final
     
-    print(f'{article} downloaded')
+    try:
+        res = cache[article]
+        print(f'{article} in cache')
+        return res
 
-    with open(DATADIR + article, 'r') as file:
-        content = file.read()
-        return content.split(',\n')
+    except KeyError:
+        print(f'{article} stored')
+
+        with open(DATADIR + article, 'r') as file:
+            content = file.read()
+            final = content.split(',\n')
+
+            cache[article] = final
+            return final
+
 
 def traverse(start):
     visited = []
     queue = []
-    
     queue.append(start)
     visited.append(start)
 
     while len(queue) > 0:
         v = queue.pop(0)
         adj = visit(v)
-        
         for i in adj:
             if not i in visited:
                 queue.append(i)
@@ -57,6 +70,6 @@ if __name__ == "__main__":
         visit('Mexico')
 
     with open('start.csv', 'r') as file:
-        starting_points = file.read().split(',')
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            [executor.submit(traverse, s) for s in starting_points]
+        starting_points = file.read().split(',')[0:2]
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            [executor.submit(traverse, start) for start in starting_points]
